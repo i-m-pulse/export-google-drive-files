460bab05cdd332de17f0a558925971ac87b364c0
Shraddha Mehta <shraddhamehta7@gmail.com>
2018-01-29 16:40:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config(object):
     OAUTH2_SCOPE = 'https://www.googleapis.com/auth/drive'
     DRIVE_CLIENT_ID = 'your drive client id'
     DRIVE_CLIENT_SECRET = 'your drive client secret'
+    DRIVE_REDIRECT_URI = 'your redirect uri'
     
     
     
